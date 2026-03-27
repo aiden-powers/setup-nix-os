01787c180e30f557db914ccf6d710ea9ec9ffec9
Aiden Powers <powersaiden07@gmail.com>
2026-03-27 16:00:07 -0700

    generate

--- setup.sh
+++ setup.sh
@@ -3,3 +3,13 @@
 sudo nix-channel --add https://nixos.org/channels/nixos-25.11 nixos
 sudo nix-channel --update
 printf "NIX_PATH: %s\n" "$NIX_PATH"
+parted /dev/sda -- mklabel gpt
+parted /dev/sda -- mkpart root ext4 512MB -8GB
+parted /dev/sda -- mkpart swap linux-swap -8GB 100%
+parted /dev/sda -- mkpart ESP fat32 1MB 512MB
+parted /dev/sda -- set 3 esp on
+mkfs.ext4 -L nixos /dev/sda1
+mkswap -L swap /dev/sda2
+mount /dev/disk/by-label/nixos /mnt
+swapon /dev/sda2
+nixos-generate-config --root /mnt
