dc9db90aa51877ff380beff12fb165f9d3c996e8
Aiden Powers <powersaiden07@gmail.com>
2026-03-27 16:02:31 -0700

    added sudo

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 sudo nix-channel --add https://nixos.org/channels/nixos-25.11 nixos
 sudo nix-channel --update
 printf "NIX_PATH: %s\n" "$NIX_PATH"
-parted /dev/sda -- mklabel gpt
-parted /dev/sda -- mkpart root ext4 512MB -8GB
-parted /dev/sda -- mkpart swap linux-swap -8GB 100%
-parted /dev/sda -- mkpart ESP fat32 1MB 512MB
-parted /dev/sda -- set 3 esp on
-mkfs.ext4 -L nixos /dev/sda1
-mkswap -L swap /dev/sda2
-mount /dev/disk/by-label/nixos /mnt
-swapon /dev/sda2
-nixos-generate-config --root /mnt
+sudo parted /dev/sda -- mklabel gpt
+sudo parted /dev/sda -- mkpart root ext4 512MB -8GB
+sudo parted /dev/sda -- mkpart swap linux-swap -8GB 100%
+sudo parted /dev/sda -- mkpart ESP fat32 1MB 512MB
+sudo parted /dev/sda -- set 3 esp on
+sudo mkfs.ext4 -L nixos /dev/sda1
+sudo mkswap -L swap /dev/sda2
+sudo mount /dev/disk/by-label/nixos /mnt
+sudo swapon /dev/sda2
+sudo nixos-generate-config --root /mnt
