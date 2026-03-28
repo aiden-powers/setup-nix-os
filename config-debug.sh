d869d0abc008ba3fcb943306326ab7b0fd62bf01
Aiden Powers <powersajbusiness@protonmail.com>
2026-03-28 08:17:32 -0700

    a

--- config-debug.sh
+++ config-debug.sh
@@ -1,6 +1,6 @@
 sudo systemctl stop nix-daemon
-sudo mv /nix/var/nix/db/db.sqlite /nix/var/nix/db/db.sqlite-bkp #https://nix.dev/guides/troubleshooting.html
-sudo sqlite3 /nix/var/nix/db/db.sqlite-bkp ".dump" | sqlite3 /nix/var/nix/db/db.sqlite
+sudo mv /nix/var/nix/db/db.sqlite /nix/var/nix/db/db.sqlite-bkp
+sqlite3 /nix/var/nix/db/db.sqlite-bkp ".dump" | sqlite3 /nix/var/nix/db/db.sqlite
 sudo systemctl start nix-daemon
 
 sudo nixos-generate-config --root /mnt
