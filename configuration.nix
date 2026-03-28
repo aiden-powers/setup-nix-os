7938a2752f14327eed1d64a165e322e3375b38d8
Aiden Powers <powersaiden07@gmail.com>
2026-03-28 02:22:05 -0700

    attempt 8 we are getting closer

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }: 
 
 {
-    imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal-combined.nix>]
+    imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal-combined.nix>];
     # graphical interface yes
     # uhhhh
 
