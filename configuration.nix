3dea6bbac08080250d9d471c8d8d92dedc9116f8
Aiden Powers <powersaiden07@gmail.com>
2026-03-27 19:22:01 -0700

    config

---
+++ configuration.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }: 
+
+{
+    imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal-combined.nix>]
+    # graphical interface yes
+    # uhhhh
+}
--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 sudo nix-channel --add https://nixos.org/channels/nixos-25.11 nixos
 sudo nix-channel --update
 printf "NIX_PATH: %s\n" "$NIX_PATH"
+
 sudo parted /dev/sda -- mklabel gpt
 sudo parted /dev/sda -- mkpart root ext4 512MB -8GB
 sudo parted /dev/sda -- mkpart swap linux-swap -8GB 100%
@@ -12,4 +13,5 @@ sudo mkfs.ext4 -L nixos /dev/sda1
 sudo mkswap -L swap /dev/sda2
 sudo mount /dev/disk/by-label/nixos /mnt
 sudo swapon /dev/sda2
+
 sudo nixos-generate-config --root /mnt
