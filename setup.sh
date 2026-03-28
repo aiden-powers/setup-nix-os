e3bcfeba9fce7ab2d66f6fcd44cac901da4b49e4
Aiden Powers <powersajbusiness@protonmail.com>
2026-03-28 16:48:49 -0700

    Adjust partition sizes in setup script

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ sudo nix-channel --update
 printf "NIX_PATH: %s\n" "$NIX_PATH"
 
 sudo parted /dev/sda -- mklabel msdos
-sudo parted /dev/sda -- mkpart primary 1MB -8GB
+sudo parted /dev/sda -- mkpart primary 1MB -32GB
 sudo parted /dev/sda -- set 1 boot on
-sudo parted /dev/sda -- mkpart primary linux-swap -8GB 100%
+sudo parted /dev/sda -- mkpart primary linux-swap -4GB 100%
 sudo mkfs.ext4 -L nixos /dev/sda1
 sudo mkswap -L swap /dev/sda2
 sudo mount /dev/disk/by-label/nixos /mnt
