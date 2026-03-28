b48e967ff83c0678ac0f155399ef562ee07e988a
Aiden Powers <powersaiden07@gmail.com>
2026-03-28 02:04:03 -0700

    attempt 4 or 5

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ sudo nix-channel --add https://nixos.org/channels/nixos-25.11 nixos
 sudo nix-channel --update
 printf "NIX_PATH: %s\n" "$NIX_PATH"
 
+nixos-generate-config --root /mnt
 sudo curl -O https://raw.githubusercontent.com/aiden-powers/setup-nix-os/refs/heads/main/configuration.nix
-sudo mv configuration.nix /etc/nixos/configuration.nix
+sudo mv configuration.nix /mnt/etc/nixos/configuration.nix
 
 #sudo wipefs -a /dev/sda # Remove: 1. partition tales, 2. filesystem signatures, 3. swap signatures
 #sudo dd if=/dev/zero of=/dev/sda bs=1M count=100 # overwrite 100mb of zeros at the beginning of the disk, make sure nothing is there so we can write to it
