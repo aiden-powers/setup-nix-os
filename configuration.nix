913d086ea52efdc51758ee35c3547f5673ff3ee1
Aiden Powers <powersajbusiness@protonmail.com>
2026-03-28 07:59:52 -0700

    make it explode

--- configuration.nix
+++ configuration.nix
@@ -1,43 +1,10 @@
-{ config, pkgs, ... }: 
+{ config, pkgs, ... }:
 
 {
-    imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal-combined.nix>];
-    # graphical interface yes
-    # uhhhh
+  imports = [ ./hardware-configuration.nix ];
 
-    networking.hostName = "powersaj";
-    time.timeZone = "America/Los_Angeles";
-    i18n.defaultLocale = "en_US.UTF-8";
-    users.users.powersaj = {
-        isNormalUser = true;
-        extraGroups = [ "wheel" "networkmanager" ];
-    };
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
 
-    networking.networkmanager.enable = true;
-
-    services = {
-        desktopManager.plasma6.enable = true;
-        displayManager.sddm.enable = true;
-        displayManager.sddm.wayland.enable = true;
-    };
-
-    environment.systemPackages = with pkgs; [
-        vim
-        git
-        wget
-        curl
-        htop
-        tmux
-        zip
-        unzip
-    ];
-
-    security.sudo.enable = true;
-
-    swapDevices = [
-        { device = "/swapfile"; }
-    ];
-
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/sda";
+  system.stateVersion = "25.11";
 }
