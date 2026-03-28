ebe14f1a22a92c737b45beb555a64e39b9b10c4e
Aiden Powers <powersajbusiness@protonmail.com>
2026-03-28 13:50:42 -0700

    changes

--- config-debug.sh
+++ config-debug.sh
@@ -9,3 +9,4 @@ set -e
 sudo nixos-generate-config --root /mnt
 sudo curl -O https://raw.githubusercontent.com/aiden-powers/setup-nix-os/refs/heads/main/configuration.nix
 sudo mv configuration.nix /mnt/etc/nixos/configuration.nix
+sudo nano /mnt/etc/nixos/configuration.nix
