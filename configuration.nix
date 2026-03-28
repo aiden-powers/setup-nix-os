01c8d1b7b12825fdf355e97f372f2d6184e32c8d
Aiden Powers <powersajbusiness@protonmail.com>
2026-03-28 08:08:51 -0700

    corrections before execute

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal-combined.nix> ];
+  imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal-combined.nix>];
 
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
